bcb999ae81b75c7240488316687527f20bf36ca3
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2024-09-13 13:19:42 +0200

    Ejercicio puntuación extra (RESUELTO)
    Solo faltaría formatear el output.

--- src/main/PruebasEjercicio4.java
+++ src/main/PruebasEjercicio4.java
@@ -6,6 +6,9 @@ import bebidas.EstrellaGalicia;
 
 import java.util.Map;
 import java.util.TreeMap;
+//Ejercicio OPCIONAL
+import java.io.File;
+import java.io.IOException;
 
 public class PruebasEjercicio4 {
 
@@ -22,9 +25,40 @@ public class PruebasEjercicio4 {
 		cervezas.put(mahou2.getReferencia(), mahou2);
 		cervezas.put(estrella2.getReferencia(), estrella2);
 		
-		System.out.println("Cervezas ordenadas por numero de referencia:\n");
+		// Ejercicio OPCIONAL
+		String rootDirectory = "CERVEZA";
+		String mahouDirectory = rootDirectory + File.separator + "MAHOU";
+		String estrellaDirectory = rootDirectory + File.separator + "ESTRELLA";
+		
+		new File(mahouDirectory).mkdirs();
+		new File(estrellaDirectory).mkdirs();
+		
+		System.out.println("Cervezas ordenadas por numero de referencia:");
 		for (Map.Entry<Integer, Cerveza> cerveza : cervezas.entrySet()) {
 			System.out.println("Referencia: " + cerveza.getKey() + "\n\t" + cerveza.getValue().toString());
+			
+			Cerveza fCerveza = cerveza.getValue();
+			String nombreArchivo = cerveza.getKey() + ".txt";
+			
+			if(fCerveza instanceof Mahou) {
+				crearArchivo(mahouDirectory, nombreArchivo);
+			} else if (fCerveza instanceof EstrellaGalicia) {
+				crearArchivo(estrellaDirectory, nombreArchivo);
+			}
+		}
+		System.out.println("\nEstructura de carpetas y archivos creada exitosamente.");
+	}
+	
+	private static void crearArchivo(String directorio, String nombreArchivo) {
+		File file = new File(directorio + File.separator + nombreArchivo);
+		try {
+			if (file.createNewFile()) {
+				System.out.println("\tArchivo creado: " + file.getName());
+			} else {
+				System.out.println("\tEl archivo ya existe.");
+			}
+		} catch (IOException e) {
+			System.out.println("Error al crear el archivo: " + e.getMessage());
 		}
 	}
 }
